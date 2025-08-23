d7a3ce73909efcf2715873f30aaffccfbd50d980
RAVI CHANDRA RAVULAPATI <rc.ravulapati@gmail.com>
2025-08-23 17:40:39 +0000

    feat: added pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import About from "./pages/about/About"
+import Contact from "./pages/contact/Contact"
+
 function App() {
 
   return (
     <>
       <h1>Ravi Chandra Ravulapati</h1>
+      <About></About>
+      <Contact></Contact>
     </>
   )
 }
---
+++ src/pages/contact/Contact.tsx
@@ -0,0 +1,16 @@
+function Contact(){
+    return (
+        <form>
+            <legend>
+                <label>Name : </label>
+                <input></input>
+            </legend>
+            <legend>
+                <label>Email address : </label>
+                <input></input>
+            </legend>
+        </form>
+    )
+}
+
+export default Contact
